59a815fefa5de187cb2bab4be5a9028856dd9bbb
Kevin <kevin.d.fay@gmail.com>
2025-06-10 23:56:36 -0400

    merge issue

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,30 +1,3 @@
-<<<<<<< Updated upstream
-import { ActionError, defineAction } from 'astro:actions';
-import { Resend } from 'resend';
-
-export const server = {
-  send: defineAction({
-    accept: 'form',
-    handler: async () => {
-      const resend = new Resend(process.env.RESEND_API_KEY);
-      const { data, error } = await resend.emails.send({
-        from: 'Acme <onboarding@resend.dev>',
-        to: ['kevin.d.fay@gmail.com'],
-        subject: 'Hello world',
-        html: '<strong>It works!</strong>',
-      });
-
-      if (error) {
-        throw new ActionError({
-          code: 'BAD_REQUEST',
-          message: error.message,
-        });
-      }
-      return data;
-    },
-  }),
-};
-=======
 import { defineAction } from 'astro:actions';
 import { z } from 'astro:schema';
 
@@ -49,4 +22,3 @@ export const server = {
     }
   })
 }
->>>>>>> Stashed changes
